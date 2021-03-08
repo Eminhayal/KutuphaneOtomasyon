da44746d5e267f6260342ae4bcbecdf106a3ee16
Emin Hayal C# <rakion_1905@hotmail.com>
2021-03-08 18:33:17 +0300

    asdsa

--- KutuphaneOtomasyon/Form1.cs
+++ KutuphaneOtomasyon/Form1.cs
@@ -16,5 +16,10 @@ namespace KutuphaneOtomasyon
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine(  "asdsa");
+        }
     }
 }
