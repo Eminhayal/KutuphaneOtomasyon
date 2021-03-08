0fb24d72aa5731543ec16198e68099a0b58cdaf1
Ali osman GÖK <osmangok.og@gmail.com>
2021-03-08 17:47:57 +0300

    kutuphane yazdım

--- KutuphaneOtomasyon/Program.cs
+++ KutuphaneOtomasyon/Program.cs
@@ -17,6 +17,7 @@ namespace KutuphaneOtomasyon
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            
         }
     }
 }
