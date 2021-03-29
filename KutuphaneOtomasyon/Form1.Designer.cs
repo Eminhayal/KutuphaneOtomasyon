2e88b18ac99bcd619fa8595144dbb3db1b85ac61
Emin Hayal C# <rakion_1905@hotmail.com>
2021-03-30 01:15:23 +0300

    asd

--- KutuphaneOtomasyon/Form1.Designer.cs
+++ KutuphaneOtomasyon/Form1.Designer.cs
@@ -35,13 +35,12 @@ namespace KutuphaneOtomasyon
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button1.Location = new System.Drawing.Point(1168, 658);
+            this.button1.Location = new System.Drawing.Point(1133, 694);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = false;
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
