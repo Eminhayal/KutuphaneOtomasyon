a80a4930c5b52de55d88c9a37a5db9ee8646bc0f
Emin Hayal C# <rakion_1905@hotmail.com>
2021-03-30 02:02:29 +0300

    asd

--- KutuphaneOtomasyon/Form1.Designer.cs
+++ KutuphaneOtomasyon/Form1.Designer.cs
@@ -32,6 +32,7 @@ namespace KutuphaneOtomasyon
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -52,12 +53,22 @@ namespace KutuphaneOtomasyon
             this.label1.TabIndex = 1;
             this.label1.Text = "çekmeeee";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1210, 429);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -72,6 +83,7 @@ namespace KutuphaneOtomasyon
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
