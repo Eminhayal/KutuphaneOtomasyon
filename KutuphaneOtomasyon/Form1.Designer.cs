820b99a39e245f368d0fa607b1096e07a0157fe2
Emin Hayal C# <rakion_1905@hotmail.com>
2021-03-30 02:21:32 +0300

    asada

--- KutuphaneOtomasyon/Form1.Designer.cs
+++ KutuphaneOtomasyon/Form1.Designer.cs
@@ -30,7 +30,17 @@ namespace KutuphaneOtomasyon
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1121, 677);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -38,6 +48,7 @@ namespace KutuphaneOtomasyon
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(1904, 1031);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "AD";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -46,6 +57,8 @@ namespace KutuphaneOtomasyon
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
