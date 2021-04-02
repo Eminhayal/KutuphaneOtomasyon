f43468d633a97d7deb6fcd3bc2bd7358db352865
Emin Hayal C# <rakion_1905@hotmail.com>
2021-04-02 22:38:15 +0300

    Güncel Hali

--- KutuphaneOtomasyon/Anasayfa.Designer.cs
+++ KutuphaneOtomasyon/Anasayfa.Designer.cs
@@ -37,6 +37,7 @@ namespace KutuphaneOtomasyon
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -123,12 +124,22 @@ namespace KutuphaneOtomasyon
             this.button7.Text = "button7";
             this.button7.UseVisualStyleBackColor = false;
             // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(882, 265);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
             // Anasayfa
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(1350, 729);
+            this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button6);
@@ -151,5 +162,6 @@ namespace KutuphaneOtomasyon
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
     }
 }
