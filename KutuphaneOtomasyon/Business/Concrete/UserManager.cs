8ed709ea1ba240262c195c47fecfb48af5815242
Emin Hayal C# <rakion_1905@hotmail.com>
2021-04-14 20:39:04 +0300

    Klasörleme Altyapısı

---
+++ KutuphaneOtomasyon/Business/Concrete/UserManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.Design;
+using KutuphaneOtomasyon.Business.Abstract;
+
+namespace KutuphaneOtomasyon.Business.Concrete
+{
+    public class UserManager : IUserService
+    {
+
+    }
+}
