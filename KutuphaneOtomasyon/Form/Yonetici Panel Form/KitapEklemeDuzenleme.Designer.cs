817c6fb35eea28f34c0409440f6bb657d18cdfa2
Emin Hayal <m195036015@ktun.edu.tr>
2021-05-13 01:52:50 +0300

    Kitap silme işlemi

--- KutuphaneOtomasyon/Form/Yonetici Panel Form/KitapEklemeDuzenleme.Designer.cs
+++ KutuphaneOtomasyon/Form/Yonetici Panel Form/KitapEklemeDuzenleme.Designer.cs
@@ -43,7 +43,7 @@ namespace KutuphaneOtomasyon
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.buttonBookDelete = new System.Windows.Forms.Button();
             this.buttonBookAdd = new System.Windows.Forms.Button();
             this.textBoxBookId = new System.Windows.Forms.TextBox();
             this.textBoxBookName = new System.Windows.Forms.TextBox();
@@ -207,19 +207,20 @@ namespace KutuphaneOtomasyon
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button4
+            // buttonBookDelete
             // 
-            this.button4.BackColor = System.Drawing.Color.Transparent;
-            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
-            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button4.ForeColor = System.Drawing.Color.Transparent;
-            this.button4.Location = new System.Drawing.Point(805, 492);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(133, 45);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Kitap Sil";
-            this.button4.UseVisualStyleBackColor = false;
+            this.buttonBookDelete.BackColor = System.Drawing.Color.Transparent;
+            this.buttonBookDelete.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonBookDelete.BackgroundImage")));
+            this.buttonBookDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonBookDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonBookDelete.ForeColor = System.Drawing.Color.Transparent;
+            this.buttonBookDelete.Location = new System.Drawing.Point(805, 492);
+            this.buttonBookDelete.Name = "buttonBookDelete";
+            this.buttonBookDelete.Size = new System.Drawing.Size(133, 45);
+            this.buttonBookDelete.TabIndex = 3;
+            this.buttonBookDelete.Text = "Kitap Sil";
+            this.buttonBookDelete.UseVisualStyleBackColor = false;
+            this.buttonBookDelete.Click += new System.EventHandler(this.buttonBookDelete_Click);
             // 
             // buttonBookAdd
             // 
@@ -301,7 +302,7 @@ namespace KutuphaneOtomasyon
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button4);
+            this.Controls.Add(this.buttonBookDelete);
             this.Controls.Add(this.buttonBookAdd);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
@@ -335,7 +336,7 @@ namespace KutuphaneOtomasyon
         private System.Windows.Forms.Button buttonBookAdd;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button buttonBookDelete;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.TextBox textBoxBookId;
