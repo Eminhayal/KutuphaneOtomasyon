a02cdf626c83d65ea4a8f81172982a2d5ac486e7
Yasin <kilic.yasn@gmail.com>
2021-05-14 01:31:44 +0300

    asd

--- KutuphaneOtomasyon/Form/Kullanici Panel Form/KitapAraAl.cs
+++ KutuphaneOtomasyon/Form/Kullanici Panel Form/KitapAraAl.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace KutuphaneOtomasyon.Form.Kullanici_Panel_Form
 {
@@ -15,6 +16,59 @@ namespace KutuphaneOtomasyon.Form.Kullanici_Panel_Form
         public KitapAraAl()
         {
             InitializeComponent();
+        }
+
+        SqlConnection baglanti = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=KitapAra;Integrated Security=True");
+        private void verilerigoster()
+        {
+            SqlConnection baglanti = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=KitapAra;Integrated Security=True");
+
+         
+            
+            SqlCommand komut = new SqlCommand("SELECT * FROM Kitap", baglanti);
+            komut.Connection = baglanti;
+            
+            baglanti.Open();
+            SqlDataReader oku = komut.ExecuteReader();
+
+            while (oku.Read())
+            {
+
+                ListViewItem ekle = new ListViewItem();
+                ekle.Text = oku["Kitap ID"].ToString();
+                ekle.SubItems.Add(oku["Kitap Adı"].ToString());
+                ekle.SubItems.Add(oku["Yazar"].ToString());
+                ekle.SubItems.Add(oku["Yayın Evi"].ToString());
+                ekle.SubItems.Add(oku["Sayfa Sayısı"].ToString());
+                ekle.SubItems.Add(oku["Kategori"].ToString());
+                ekle.SubItems.Add(oku["Raf / Salon"].ToString());
+                ekle.SubItems.Add(oku["Durum"].ToString());
+
+                listView1.Items.Add(ekle);
+
+
+
+            }
+            baglanti.Close();
+
+
+
+        }
+
+
+        private void KitapAraAl_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            verilerigoster();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
